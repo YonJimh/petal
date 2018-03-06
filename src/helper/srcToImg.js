089178458c7f2ee3d803af32faed14f006c500f8
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-03-06 13:38:54 +0800

    asd

--- src/helper/srcToImg.js
+++ src/helper/srcToImg.js
@@ -6,11 +6,11 @@ const {promisify} = require('util');
 const writeFile = promisify(fs.watchFile);
 
 module.exports =  urlToImg = promisify((url, dir,callback) => {
-    const mod = /^https:/.test(url) ? https : http; //判断哪个协议
+    
     // const ext = path.extname(url);
     const file = path.join(dir, `${Date.now()}.jpg`);
 
-    mod.get(url, res => {
+    https.get(url, res => {
         res.pipe(fs.createWriteStream(file))
         .on('finish',()=>{
             callback();  //设了回调 用promise让他好看点
