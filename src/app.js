43422eb7a252d2a899b87f49dba7dd2b7989d069
yongjian-h <33388713+yongjian-h@users.noreply.github.com>
2018-03-07 13:25:49 +0800

    das

--- src/app.js
+++ src/app.js
@@ -4,33 +4,34 @@ const srcToImg = require('./helper/srcToImg');
 
 (async ()=>{
     const browser = await puppeteer.launch({
-        // headless:false,
+        headless:false,
         timeout:0
     });
     const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.on('request', interceptedRequest=>{
-        interceptedRequest.continue({
-            header:{
-                'Host': 'huaban.com',
-                'Accept': 'application/json',
-                'X-Request': 'JSON',
-                'X-Requested-With': 'XMLHttpRequest'
-            }
-        })
-    });
-    await page.goto('http://huaban.com/',{waitUntil: 'networkidle2',timeout:0});
+    conf.search = '背景';
+    await page.goto(`http://huaban.com/search/?q=${conf.search}&jegmfkl9&page=2&per_page=40&wfl=1`,{waitUntil: 'networkidle2',timeout:0});
     // await page.setViewport({
     //     height:5000,
     //     width:1920
     // });
     // await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
 
-    await page.focus('#query');
-    await page.keyboard.sendCharacter(conf.search);
-    // await browser.close();
-    await page.click('a.go');
-
+    // await page.focus('#query');
+    // await page.keyboard.sendCharacter(conf.search);
+    // // await browser.close();
+    // await page.click('a.go');
+    // await page.setRequestInterception(true);
+    // page.on('request', interceptedRequest=>{
+    //     interceptedRequest.continue({
+    //         header:{
+                
+    //             'Host': 'huaban.com',
+    //             'Accept': 'application/json',
+    //             'X-Request': 'JSON',
+    //             'X-Requested-With': 'XMLHttpRequest'
+    //         }
+    //     })
+    // });
     page.on('load',async ()=>{ 
         srcs = await page.evaluate(()=>{
             // const list=[];
